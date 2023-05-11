0f38b7495db513deb05dab9b976f4ba9cba3407c
AlFalahTaieb <taiebalfalah@gmail.com>
2023-05-11 11:35:15 +0200

    update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
+
 green="\e[1;32m"
 red="\e[1;31m"
 undim="\e[0m"
 
-# WILL LIST ALL THE DETECTED WIFI
-nmcli device wifi list
-# SHOW A QR OF THE CURRENT CONNEXION
-nmcli device wifi show-password
-#SHOW IF VPN IS ACTIVE OR NOT ON THE CURRENT SESSION
-active=$(nmcli con show --active | grep -e mullvad -e tun0)
-if [ -z "$active" ]
-    then echo "VPN Not Active"
-    content=$(curl -sS "https://ipleak.net/json/")
-    echo $content | jq -r '. | "IP: \(.ip) *** Country: \(.country_name) ** City: \(.city_name)  ** TIMEZONE: \(.time_zone)"'
-else 
-    echo "VPN Active"
-    content=$(curl -sS "https://ipleak.net/json/")
-    echo $content | jq -r '. | "IP: \(.ip) *** Country: \(.country_name) ** City: \(.city_name)  ** TIMEZONE: \(.time_zone)"'
+echo "What would you like to do? Please select an option:"
+echo "1. Display a list of detected WiFi networks"
+echo "2. Display the password of the current WiFi network"
+echo "3. Check if VPN is active and display geo-location information"
+read choice
+
+# Ask the user for inputs based on the selected choice
+if [ "$choice" = "1" ]; then
+  nmcli device wifi list
+elif [ "$choice" = "2" ]; then
+  nmcli device wifi show-password
+elif [ "$choice" = "3" ]; then
+  active=$(nmcli con show --active | grep -e mullvad -e tun0)
+  if [ -z "$active" ]; then
+    echo "VPN not active"
+  else
+    echo "VPN active"
+  fi
+  content=$(curl -sS "https://ipleak.net/json/")
+  echo $content | jq -r '. | "IP: \(.ip) *** Country: \(.country_name) ** City: \(.city_name)  ** Timezone: \(.time_zone)"'
+else
+  echo "Invalid choice, please select between 1, 2 or 3."
+  exit 1
 fi
-
-
